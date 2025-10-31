ba7f42b3ee7d241427f0cca45d7af60b58d5678a
WaqarGitNorthrays <waqar@northrays.com>
2025-10-31 11:29:16 +0500

    baseurl update

--- src/utils/adminApi.js
+++ src/utils/adminApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const adminApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://app.theplantsmall.com/",
+  baseURL: "https://app.theplantsmall.com/",
   headers: {
     "Content-Type": "application/json",
   },
